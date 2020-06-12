ae1811b443ae7d878d8a0eedac58e2167387ee54
DELL <DELL@192.168.0.6>
2020-06-12 13:16:06 +0530

    ifelseEx1

---
+++ Milestone1/src/com/wipro/ifelsestatement/Ex1.java
@@ -0,0 +1,16 @@
+package com.wipro.ifelsestatement;
+
+public class Ex1 {
+      public static void main(String[] args) {
+		int a=Integer.parseInt(args[0]);
+		if(a>0) {
+		System.out.println("Positive");
+		}
+		if(a<0) {
+			System.out.println("Negative");
+			}
+		if(a==0) {
+			System.out.println("Zero");
+			}
+	}
+}
