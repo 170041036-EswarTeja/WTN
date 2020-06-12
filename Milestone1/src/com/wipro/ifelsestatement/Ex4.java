9f5dbfd23f366085ba1b612ff83da600d6854adc
DELL <DELL@192.168.0.6>
2020-06-12 17:59:22 +0530

    ifelseEx4

---
+++ Milestone1/src/com/wipro/ifelsestatement/Ex4.java
@@ -0,0 +1,16 @@
+package com.wipro.ifelsestatement;
+
+public class Ex4 {
+        public static void main(String[] args) {
+			char a= args[0].charAt(0);
+			char b= args[1].charAt(0);
+			if(a>b)
+			{
+				System.out.println(a+","+b);
+			}
+			else
+			{
+				System.out.println(b+","+a);
+			}
+		}
+}
