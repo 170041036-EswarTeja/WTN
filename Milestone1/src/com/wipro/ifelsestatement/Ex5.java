58b9aad3efaae83266e4ded37a330ddd3a0a91db
DELL <DELL@192.168.0.6>
2020-06-12 18:58:16 +0530

    ifelseEx5

---
+++ Milestone1/src/com/wipro/ifelsestatement/Ex5.java
@@ -0,0 +1,19 @@
+package com.wipro.ifelsestatement;
+
+public class Ex5 {
+        public static void main(String[] args) {
+			char c=args[0].charAt(0);
+			if((c>=65&&c<=90)||(c>=97&&c<=122))
+			{
+				System.out.println("Alphabet");
+			}
+			if(c>=48&&c<=57)
+			{
+				System.out.println("Digit");
+			}
+			else
+			{
+				System.out.println("Special Character");
+			}
+		}
+}
