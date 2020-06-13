b39977a7c431f9ca530b2e04f94829192f1ea411
DELL <DELL@192.168.0.6>
2020-06-13 14:03:13 +0530

    loopingEx4

---
+++ Milestone1/src/com/wipro/loopingstatement/Ex4.java
@@ -0,0 +1,22 @@
+package com.wipro.loopingstatement;
+
+public class Ex4 {
+          public static void main(String[] args) {
+      			int c;
+      			for(int i=10;i<99;i++)
+      			{    
+      				c=0;
+      				for(int j=2;j<=i;j++)
+      				{	
+      				if(i%j==0)
+      				{
+      					c++;
+      				}
+      				}
+      				if(c==1)
+      				{
+      					System.out.println(i);
+      				}
+      			}
+		}
+}
