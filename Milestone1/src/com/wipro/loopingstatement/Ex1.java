edc9a77f91cc713dbb34f7b3813ab43a8ddff67d
DELL <DELL@192.168.0.6>
2020-06-13 12:58:16 +0530

    loopingEx1

---
+++ Milestone1/src/com/wipro/loopingstatement/Ex1.java
@@ -0,0 +1,10 @@
+package com.wipro.loopingstatement;
+
+public class Ex1 {
+           public static void main(String[] args) {
+			for(int i=1;i<=10;i++)
+			{
+				System.out.print(i+" ");
+			}
+		}
+}
